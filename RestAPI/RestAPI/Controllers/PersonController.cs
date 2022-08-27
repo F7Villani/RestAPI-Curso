b4774d318016d40a7048f114b12304b225bf9d38
Felipe Villani <felipevillani.2000@hotmail.com>
2022-08-27 05:50:34 -0300

    feat: criando e implementando endpoints

--- RestAPI/RestAPI/Controllers/PersonController.cs
+++ RestAPI/RestAPI/Controllers/PersonController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestAPI.Services;
+using RestAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,17 +15,57 @@ namespace RestAPI.Controllers
     {
 
         private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
 
-        public PersonController(ILogger<PersonController> logger)
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
         {
             _logger = logger;
+            _personService = personService;
+
         }
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(" === Calculadora ===");
+            return Ok(_personService.GetAll());
         }
 
+        [HttpGet("(id)")]
+        public IActionResult Get(long id)
+        {
+            Person person = _personService.GetById(id);
+            if(person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+        }
+        
+        [HttpPost]
+        public IActionResult Create([FromBody] Person person)
+        {
+            if(person == null)
+            {
+                return BadRequest();
+            }
+            return Ok(_personService.Create(person));
+        }
+        
+        [HttpPut]
+        public IActionResult Update([FromBody] Person person)
+        {
+            if(person == null)
+            {
+                return BadRequest();
+            }
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("(id)")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
     }
 }
