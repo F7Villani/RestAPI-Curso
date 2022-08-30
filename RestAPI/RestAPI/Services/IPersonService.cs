138b14a937ac81e0f797814fc28984ccbec6f74b
Felipe Villani <felipevillani.2000@hotmail.com>
2022-08-30 19:15:49 -0300

    git: arrumando .gitignore

---
+++ RestAPI/RestAPI/Services/IPersonService.cs
@@ -0,0 +1,21 @@
+﻿using RestAPI.Models;
+using System.Collections.Generic;
+
+namespace RestAPI.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person GetById(long id);
+
+        Person Update(Person person);
+
+        List<Person> GetAll();
+
+        void Delete(long id);
+
+
+
+    }
+}
