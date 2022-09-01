3c7326493911fe076b530690f949cbaae75ac401
Felipe Villani <felipevillani.2000@hotmail.com>
2022-08-31 21:37:40 -0300

    feat: adicionando referencias ao BD

--- RestAPI/RestAPI/Models/Person.cs
+++ RestAPI/RestAPI/Models/Person.cs
@@ -1,15 +1,24 @@
-﻿namespace RestAPI.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestAPI.Models
 {
+    // Referência da tabela no BD
+    [Table("people")]
     public class Person
     {
+        [Column("id")]
         public long Id { get; set; }
 
+        [Column("firstName")]
         public string FirstName { get; set; }
 
+        [Column("lastName")]
         public string LastName { get; set; }
 
+        [Column("address")]
         public string Address { get; set; }
 
+        [Column("gender")]
         public string Gender { get; set; }
     }
 }
