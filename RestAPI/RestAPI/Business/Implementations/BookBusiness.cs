6777dc102c5c6c9764536a1ab4fd9c30b5d10c13
Felipe Villani <felipevillani.2000@hotmail.com>
2022-09-08 16:23:26 -0300

    feat: criando e implementando book business

---
+++ RestAPI/RestAPI/Business/Implementations/BookBusiness.cs
@@ -0,0 +1,42 @@
+﻿using RestAPI.Models;
+using RestAPI.Repository;
+using System.Collections.Generic;
+
+namespace RestAPI.Business.Implementations
+{
+    public class BookBusiness : IBookBusiness
+    {
+        private IBookRepository _repository;
+
+        public BookBusiness(IBookRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Book> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public Book GetById(long id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public Book Create(Book book)
+        {
+            return _repository.Create(book);
+        }
+        
+        public Book Update(Book book)
+        {
+            return _repository.Update(book);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+    }
+}
