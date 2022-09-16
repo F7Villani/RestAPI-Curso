3bb53091a4c8349083e0dadc12408a938af32de9
Felipe Villani <felipevillani.2000@hotmail.com>
2022-09-15 21:11:49 -0300

    init: implementando PersonConverter

---
+++ RestAPI/RestAPI/Data/Contract/Implementations/PersonConverter.cs
@@ -0,0 +1,48 @@
+﻿using RestAPI.Data.Converter.VO;
+using RestAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestAPI.Data.Contract.Implementations
+{
+    public class PersonConverter : IParser<PersonVO, Person>, IParser<Person, PersonVO>
+    {
+        public PersonVO Parse(Person origin)
+        {
+            if (origin == null) return null;
+            return new PersonVO
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                Address = origin.Address,
+                Gender = origin.Gender
+            };
+        }
+
+        public List<PersonVO> Parse(List<Person> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(person => Parse(person)).ToList();
+        }
+
+        public Person Parse(PersonVO origin)
+        {
+            if (origin == null) return null;
+            return new Person
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                Address = origin.Address,
+                Gender = origin.Gender
+            };
+        }
+
+        public List<Person> Parse(List<PersonVO> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(personVO => Parse(personVO)).ToList();
+        }
+    }
+}
