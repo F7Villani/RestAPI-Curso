d73dfe75796dd5bf41f3c2d09801713f4cef22b0
Felipe Villani <felipevillani.2000@hotmail.com>
2022-09-19 21:26:30 -0300

    feat: habilitando CORS

--- RestAPI/RestAPI/Startup.cs
+++ RestAPI/RestAPI/Startup.cs
@@ -33,6 +33,12 @@ namespace RestAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddDefaultPolicy(builder =>
+            {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyMethod();
+                builder.AllowAnyHeader();
+            }));
 
             services.AddControllers();
 
@@ -101,6 +107,8 @@ namespace RestAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseSwagger();
 
